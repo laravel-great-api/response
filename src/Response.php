6cb3fb0a608ca40d6f597b08d60e143a856c026e
walshdev <alexwalsh.dev@gmail.com>
2022-10-13 02:21:57 +0300

    Refactor

--- src/Response.php
+++ src/Response.php
@@ -3,139 +3,245 @@
 namespace LaravelGreatApi\Response;
 
 use Illuminate\Contracts\Support\Responsable;
-use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 /**
- * @method mixed statusText
- * @method mixed data
+ * @method string statusText()
+ * @method array data()
  */
 class Response implements Responsable
 {
-	private mixed $data = null;
-
-	private int $status = HttpResponse::HTTP_OK;
-
-	private $errorMessage = null;
-
-	private array $headers = [];
-
-	public function __construct(mixed $data, int $status = HttpResponse::HTTP_OK, $headers = [])
-	{
-		$this->data = $data;
-		$this->status = $status;
-		$this->headers = $headers;
-	}
+    private array $errorMessages = [
+        //
+    ];
 
 	/**
-	 * Undocumented function
+	 * Undocumented variable
 	 *
-	 * @return integer
+	 * @var array|null
 	 */
-	protected function getStatusCode(): int
-	{
-		return $this->status;
-	}
-
-	private function getData()
-	{
-		if (method_exists($this, 'data')) {
-			return $this->data();
-		}
-
-		return $this->data;
-	}
+	private ?array $arguments = null;
 
 	/**
-	 * Is Response Success
+	 * Constructor
 	 *
-	 * @return boolean
+	 * @param array $arguments
 	 */
-	private function isResponseSuccess(): bool
+	public function __construct($arguments = [])
 	{
-		return $this->getStatusCode() >= 200 && $this->getStatusCode() < 300;
+		$this->arguments = is_array($arguments) ? $arguments : func_get_args();
 	}
 
-	/**
-	 * Undocumented function
-	 *
-	 * @return array
-	 */
-	private function defaultHeaders(): array
-	{
-		return ['Content-Type' => 'application/json'];
-	}
+    /**
+     * Undocumented function
+     *
+     * @param string $method
+     * @return mixed
+     */
+    private function callMethod(string $method)
+    {
+        if (\method_exists($this, $method)) {
+            if ($this->arguments) {
+                return $this->{$method}(...array_values($this->arguments));
+            }
 
-	/**
-	 * Undocumented function
-	 *
-	 * @return array
-	 */
-	private function getHeaders(): array
-	{
-		return array_merge($this->headers, $this->defaultHeaders());
-	}
+            return $this->{$method}();
+        }
 
-	private function getTextStatus()
-	{
-		if (method_exists($this, 'statusText')) {
-			return $this->statusText();
-		}
+        return null;
+    }
 
-		return null;
-	}
+    /**
+     * Is response invalid?
+     *
+     * @see https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+     *
+     * @final
+     */
+    public function isInvalid(): bool
+    {
+        return $this->getStatusCode() < 100 || $this->getStatusCode() >= 600;
+    }
 
-	protected function setErrorMessage($message)
-	{
-		$this->errorMessage = $message;
-	}
+    /**
+     * Is response informative?
+     *
+     * @final
+     */
+    public function isInformational(): bool
+    {
+        return $this->getStatusCode() >= 100 && $this->getStatusCode() < 200;
+    }
 
-	protected function setStatus($status)
-	{
-		$this->status = $status;
-	}
+    /**
+     * Is response successful?
+     *
+     * @final
+     */
+    public function isSuccessful(): bool
+    {
+        return $this->getStatusCode() >= 200 && $this->getStatusCode() < 300;
+    }
 
-	/**
-	 * Undocumented function
-	 *
-	 * @return array
-	 */
-	private function toArray(): array
-	{
-		if ($this->status === HttpResponse::HTTP_NO_CONTENT) {
-			return [];
-		}
+    /**
+     * Is the response a redirect?
+     *
+     * @final
+     */
+    public function isRedirection(): bool
+    {
+        return $this->getStatusCode() >= 300 && $this->getStatusCode() < 400;
+    }
 
-		return array_filter([
-			'error' => $this->getTextStatus(),
-			'message' => $this->errorMessage,
-			'data' => $this->getData(),
-			'success' => $this->isResponseSuccess(),
-			'status' => $this->getStatusCode()
-		], fn($value) => !is_null($value));
-	}
+    /**
+     * Is there a client error?
+     *
+     * @final
+     */
+    public function isClientError(): bool
+    {
+        return $this->getStatusCode() >= 400 && $this->getStatusCode() < 500;
+    }
 
-	/**
-	 * Undocumented function
-	 *
-	 * @return string|false
-	 */
-	private function toJson(): string|false
-	{
-		return json_encode($this->toArray());
-	}
+    /**
+     * Was there a server side error?
+     *
+     * @final
+     */
+    public function isServerError(): bool
+    {
+        return $this->getStatusCode() >= 500 && $this->getStatusCode() < 600;
+    }
 
-	/**
-	 * Undocumented function
-	 *
-	 * @param [type] $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function toResponse($request)
-	{
-		return new HttpResponse(
-			$this->toJson(),
-			$this->getStatusCode(),
-			$this->getHeaders()
-		);
-	}
+    /**
+     * Is the response OK?
+     *
+     * @final
+     */
+    public function isOk(): bool
+    {
+        return 200 === $this->getStatusCode();
+    }
+
+    /**
+     * Is the response forbidden?
+     *
+     * @final
+     */
+    public function isForbidden(): bool
+    {
+        return 403 === $this->getStatusCode();
+    }
+
+    /**
+     * Is the response a not found error?
+     *
+     * @final
+     */
+    public function isNotFound(): bool
+    {
+        return 404 === $this->getStatusCode();
+    }
+
+    /**
+     * Is the response a redirect of some form?
+     *
+     * @final
+     */
+    public function isRedirect(string $location = null): bool
+    {
+        return \in_array($this->getStatusCode(), [201, 301, 302, 303, 307, 308]) && (null === $location ?: $location == $this->headers->get('Location'));
+    }
+
+    /**
+     * Is the response empty?
+     *
+     * @final
+     */
+    public function isEmpty(): bool
+    {
+        return \in_array($this->getStatusCode(), [204, 304]);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return integer
+     */
+    private function getStatusCode(): int
+    {
+        return $this->callMethod('status') ?? HttpFoundationResponse::HTTP_OK;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return mixed
+     */
+    private function getStatusText()
+    {
+        if ($this->isSuccessful() === false) {
+            return $this->callMethod('statusText') ?? HttpFoundationResponse::$statusTexts[$this->getStatusCode()];
+        }
+
+        return null;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return string|null
+     */
+    private function getErrorMessage(): ?string
+    {
+        if ($this->isSuccessful() === false) {
+            return $this->callMethod('errorMessage') ?? $this->errorMessages[$this->getStatusCode()] ?? "";
+        }
+
+        return null;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return array|null
+     */
+    private function getData(): ?array
+    {
+        if ($this->isSuccessful()) {
+            return $this->callMethod('data') ?? [];
+        }
+
+        return null;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $request
+     * @return mixed
+     */
+    public function toArray($request)
+    {
+        return array_filter([
+            'error' => $this->getStatusText(),
+            'message' => $this->getErrorMessage(),
+            'data' => $this->getData(),
+            'success' => $this->isSuccessful(),
+            'status' => $this->getStatusCode()
+        ], fn($value) => !is_null($value));
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param \Illuminate\Http\Response $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        return new JsonResponse($this->toArray($request));
+    }
 }
